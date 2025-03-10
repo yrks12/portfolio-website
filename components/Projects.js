82f74dd9bebd9afed929004c87b00e6005ddb149
yrks12 <128397979+yrks12@users.noreply.github.com>
2025-03-10 20:53:09 +0000

    Add Projects component for portfolio website

---
+++ components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <section id="projects">
+            <h2>Projects</h2>
+            <div className="project-grid">
+             {/* Project cards will be mapped here */}
+            </div>
+        </section>
+    );
+};
+
+export default Projects;
