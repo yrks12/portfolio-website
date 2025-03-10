eb41d4a336eb6f3cb9cd0eb76276fbf98ae5320b
yrks12 <128397979+yrks12@users.noreply.github.com>
2025-03-10 20:34:54 +0000

    Create AboutMe component for portfolio website.

---
+++ components/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <section id="about-me">
+            <h2>About Me</h2>
+            <p>Information about the client.</p>
+        </section>
+    );
+};
+
+export default AboutMe;
