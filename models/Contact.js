33b38895d75e3de28833390601c717f551415b58
yrks12 <128397979+yrks12@users.noreply.github.com>
2025-03-10 20:18:42 +0000

    Add Contact model for MongoDB

---
+++ models/Contact.js
@@ -0,0 +1,19 @@
+// models/Contact.js
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Contact', ContactSchema);
