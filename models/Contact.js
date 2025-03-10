58f9fa86dcafa5dff301c2017d3d916c67f54df5
yrks12 <128397979+yrks12@users.noreply.github.com>
2025-03-10 20:53:37 +0000

    Create Contact.js model for contact messages.

--- models/Contact.js
+++ models/Contact.js
@@ -1,19 +1,9 @@
-// models/Contact.js
 const mongoose = require('mongoose');
 
-const ContactSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    message: {
-        type: String,
-        required: true
-    }
+const contactSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    message: { type: String, required: true },
 }, { timestamps: true });
 
-module.exports = mongoose.model('Contact', ContactSchema);
+module.exports = mongoose.model('Contact', contactSchema);
